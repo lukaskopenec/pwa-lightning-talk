5adfc00d42d08859cdfc553d6b8be2bdd22f9f82
Lukas Kopenec <lukas.kopenec@outlook.cz>
2021-05-13 14:24:39 +0200

    Add presentation.

--- client/src/app/presentation/presentation.component.ts
+++ client/src/app/presentation/presentation.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+const SLIDES_COUNT = 11;
 
 @Component({
   selector: 'app-presentation',
   templateUrl: './presentation.component.html',
   styleUrls: ['./presentation.component.scss']
 })
-export class PresentationComponent implements OnInit {
+export class PresentationComponent {
+  currentSlideId = 1;
+  maxSlides = SLIDES_COUNT;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  get currentSlideUrl(): string {
+    return `assets/img/Snímek${this.currentSlideId}.PNG`;
   }
 
+  onNextSlide(): void {
+    if (this.currentSlideId < SLIDES_COUNT) {
+      ++this.currentSlideId;
+    }
+  }
+
+  onPrevSlide(): void {
+    if (this.currentSlideId > 1) {
+      --this.currentSlideId;
+    }
+  }
+
+  onFirstSlide(): void {
+    this.currentSlideId = 1;
+  }
+
+  onLastSlide(): void {
+    this.currentSlideId = SLIDES_COUNT;
+  }
 }
